07c37386a9529240d2dd9bad066a71ed4af41258
daniel on linux <daniel.on.web@gmail.com>
2015-12-13 03:35:36 +0200

    added Timer class to common.h

--- urlmatching2/src/common.h
+++ urlmatching2/src/common.h
@@ -30,12 +30,59 @@ int get_curr_memsize()
 	return mem;
 }
 
-#else
+#else	//windows os
 //Only valid for unix systems
 inline
 int get_curr_memsize() { return 0; }
 #endif
 
+
+#define GETTIMING double(end - begin) / (CLOCKS_PER_SEC)
+#define START_TIMING 	do {begin = std::clock();} while(0)
+#define STOP_TIMING 	do {end = std::clock();} while(0)
+#define PREPARE_TIMING clock_t begin,end
+
+/*
+ * Unix/Windows
+ * Timer:
+ */
+class Timer {
+public:
+	Timer(bool start=true):
+		_begin(0),_time(0l),_running(false)
+	{
+		if (start)
+			start();
+	}
+	virtual ~Timer() {}
+
+	void reset()	{
+		_time = 0;
+		_begin = 0;
+		_running = false;
+	}
+	void start() 	{
+		_begin = std::clock();
+		_running = true;
+	}
+	void stop()		{
+		clock_t end = std::clock();
+		if (_running) {
+			_time+= (end - _begin);
+			_running = false;
+		}
+	}
+	// Stop and get_seconds
+	double get_seconds()	{
+		stop();
+		return double ( double (_time) / (CLOCKS_PER_SEC) );
+	}
+
+private:
+	clock_t _begin;
+	clock_t _time;
+	bool _running;
+};
 
 
 
